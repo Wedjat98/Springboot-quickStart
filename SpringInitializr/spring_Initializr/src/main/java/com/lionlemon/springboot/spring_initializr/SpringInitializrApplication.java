f231f1bd8c55f53aa1d4a0163f2e6368fc15109b
lionlemon <ame9807@gmail.com>
2021-06-16 00:33:17 +0900

    springboot 的父模块和子模块

---
+++ SpringInitializr/spring_Initializr/src/main/java/com/lionlemon/springboot/spring_initializr/SpringInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.lionlemon.springboot.spring_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInitializrApplication.class, args);
+    }
+
+}
---
+++ SpringInitializr/spring_Initializr/src/test/java/com/lionlemon/springboot/spring_initializr/SpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionlemon.springboot.spring_initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
