431811c2dde74f4e483aab031155cd6474db6fa7
lionlemon <ame9807@gmail.com>
2021-06-17 21:32:05 +0900

    测试springboot之扩展SpringMVC完结

--- thymeleafdemo/src/main/java/com/lionlemon/thymeleafdemo/controller/TestController.java
+++ thymeleafdemo/src/main/java/com/lionlemon/thymeleafdemo/controller/TestController.java
@@ -3,11 +3,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class TestController {
     @RequestMapping("/test")
     public String test(Model model){
         model.addAttribute("msg","你好，这里是testController");
+        model.addAttribute("users", Arrays.asList("zhu1","zhu2"));
+
+
         return "test";
     }
 }
