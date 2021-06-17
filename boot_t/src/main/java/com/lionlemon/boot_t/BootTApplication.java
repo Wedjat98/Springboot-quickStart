c62fd5e89416d51d78694b78386ee936a267775e
lionlemon <ame9807@gmail.com>
2021-06-17 20:40:59 +0900

    测试springboot之thymeleaf的新模块

---
+++ boot_t/src/main/java/com/lionlemon/boot_t/BootTApplication.java
@@ -0,0 +1,13 @@
+package com.lionlemon.boot_t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootTApplication.class, args);
+    }
+
+}
---
+++ boot_t/src/test/java/com/lionlemon/boot_t/BootTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionlemon.boot_t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
